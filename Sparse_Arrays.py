98d2d4238e81c92fa7dc0152a39ca2f612d25102
Daniel Hernández Vicente <newrol@users.noreply.github.com>
2022-02-16 10:00:48 +0100

    Create Sparse_Arrays.py

---
+++ Sparse_Arrays.py
@@ -0,0 +1,62 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    # Write your code here
+    
+    frecuency_map = {}
+    response = []
+    
+    for string in strings:
+        if string in frecuency_map:
+            frecuency_map[string] += 1
+        else:
+            frecuency_map[string] = 1
+
+    for query in queries:
+        if query in frecuency_map:
+            response.append(frecuency_map[query])
+        else:
+            response.append(0)
+        
+    return response
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
