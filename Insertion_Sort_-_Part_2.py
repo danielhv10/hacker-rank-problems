ea10aaf2251c335586fea7444860c865ffa32a7a
Daniel Hernández Vicente <newrol@users.noreply.github.com>
2022-02-25 13:41:55 +0100

    Create Insertion_Sort_-_Part_2.py

---
+++ Insertion_Sort_-_Part_2.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort2' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort2(n, arr):
+    # Write your code here
+    
+    for index_i  in range(1,n):
+        
+        index_p = index_i -1
+        
+        while arr[index_i] < arr[index_p] and index_p >= 0:
+            index_p -=1
+        
+        if index_p < index_i -1:
+            
+            aux = arr[index_i]
+            
+            for index_j in range(index_i, index_p, -1):
+            
+               arr[index_j] = arr[index_j -1]
+                       
+            arr[index_p+1] = aux
+       
+        print(' '.join(map(str, arr)))               
+         
+        
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort2(n, arr)
